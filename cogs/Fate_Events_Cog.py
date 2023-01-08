a4ec7f9278b4160a2f6598e72caf2bb530704d8a
Iago Barbosa <iagofbarbosa23@gmail.com>
2023-01-08 05:18:52 -0300

    Adição de DocStrings

--- cogs/Fate_Events_Cog.py
+++ cogs/Fate_Events_Cog.py
@@ -10,18 +10,24 @@ class FateEvents(commands.Cog):
     def __init__(self: object, bot: discord.client) -> None:
         self.bot = bot
         self.database: RedisDatabase = RedisDatabase.get_database()
-        self.controller = FateEventsController()
-        self.logger = Log.get_logger()
+        self.controller: FateEventsController = FateEventsController()
+        self.logger: Log = Log.get_logger()
         self.verify_current_event_and_post_it.start()
     
     # """hours= 24"""
     @tasks.loop(hours=12)
     async def verify_current_event_and_post_it(self: object) -> None:
+        """
+        This method does the verification of the current event's of the FGO game, and if there is any event it will be posted
+
+        Args:
+            self (object): The object itself
+        """
         try:
             self.logger.info("Running verification of events of FGO Game")
-            actual_events = self.controller.get_events_from(datetime.datetime(year=2022, month=12, day=1))
+            actual_events = self.controller.get_events_from(datetime.datetime.now())
             if len(actual_events) >= 1:
-                await self.controller.post_actual_events(actual_events, self.bot)
+                await self.controller.post_events(actual_events, self.bot)
         except Exception as ex:
             self.logger.error(ex)
 
--- controller/FateEventsController.py
+++ controller/FateEventsController.py
@@ -12,10 +12,21 @@ class FateEventsController:
         self.database: Redis = Redis.get_database()
     
     def get_events_from(self: object, events_start_date: datetime = datetime.datetime.now(), events_end_date: datetime = datetime.datetime.now()) -> tuple:
+        """
+        This method get events from Atlas Academy API with a range of date that is passed as a parameter
+
+        Args:
+            self (object): 
+            events_start_date (datetime, optional): The beginning date of the event. Defaults to datetime.datetime.now().
+            events_end_date (datetime, optional): The end date of the event. Defaults to datetime.datetime.now().
+
+        Returns:
+            tuple: _description_
+        """
         try:
             raw_events_file = requests.get('https://api.atlasacademy.io/export/NA/nice_war.json')
             fgo_events_json = json.loads(raw_events_file.content.decode('utf8'))
-            actual_events = self.get_actual_events_from(fgo_events_json, events_start_date, events_end_date)
+            actual_events = self.filter_events_from(fgo_events_json, events_start_date, events_end_date)
             actual_events = tuple(FateGOEvent(  id=event['id'],
                                                 name=event['eventName'],
                                                 start_date=datetime.datetime.fromtimestamp(event['warAdds'][0]['startedAt']),
@@ -25,23 +36,65 @@ class FateEventsController:
         except Exception as ex:
             self.logger.error('Exceção durante a coleta de eventos.', ex)
 
-    def get_actual_events_from(self: object, fgo_events: tuple, start_date, end_date) -> tuple:
+    def filter_events_from(self: object, fgo_events: tuple, start_date, end_date) -> tuple:
+        """
+        This method filters the events that are occurring between the given dates
+
+        Args:
+            self (object): _description_
+            fgo_events (tuple): List of events to apply the filter
+            start_date (_type_): The start date to be used as a filter parameter
+            end_date (_type_): The end date to be used as a filter parameter
+
+        Returns:
+            tuple: An tuple of events that are occurring between the given dates
+        """
         events = tuple(filter(lambda event: len(event['warAdds']) > 0 and datetime.datetime.fromtimestamp(event['warAdds'][0]['startedAt']) >= start_date and datetime.datetime.fromtimestamp(event['warAdds'][0]['endedAt']) <= end_date, fgo_events))
         special_events_filtered = tuple(event for event in fgo_events if event['id'] == 9999)[0]['spots'][0]['quests']
-        special_things = tuple(event for event in special_events_filtered if '2023' in event['name'])
         special_events = tuple(filter(lambda event: datetime.datetime.fromtimestamp(event['openedAt']) >= start_date and datetime.datetime.fromtimestamp(event['closedAt']) <= end_date, special_events_filtered))
         return (events + special_events)
     
-    async def post_actual_events(self: object, events: tuple, bot) -> None:
+    async def post_events(self: object, events: tuple, bot) -> None:
+        """
+        This method is called to post Events
+
+        Args:
+            self (object): _description_
+            events (tuple): The events that will be posted
+            bot (_type_): The discord client
+        """
         try:
-            guilds = self.prepare_guilds_for_action(bot)
+            guilds = self.get_active_guilds(bot)
             messages = FateEventsController.prepare_messages_about(events)
             await self.send_messages(guilds, messages)
         except Exception as ex:
             self.logger.error('Erro while trying to post events', ex)
 
+    def get_active_guilds(self: object, bot) -> tuple:
+        """
+        Get the current guilds where the bot is located, and are active.
+
+        Args:
+            self (object): _description_
+            bot (_type_): _description_
+
+        Returns:
+            tuple: tuple of guild's 
+        """
+        guilds = self.database.get_data_list('bot:guilds:*')
+        return tuple(bot.get_guild(int(guild_id)) for guild_id in guilds.values())
+
     @staticmethod
     def prepare_messages_about(events: tuple) -> list:
+        """
+        This method create messages about the events, so it can be posted on the Discord Guilds
+
+        Args:
+            events (tuple): Tuple of events who's gonna be posted
+
+        Returns:
+            list: List of events Embed messages
+        """
         messages = []
         for event in events:
             message = Embed(color=0xff8080)
@@ -51,18 +104,21 @@ class FateEventsController:
         return messages
 
     async def send_messages(self: object, guilds: tuple, messages: tuple) -> None:
+        """
+        Sends the given messages to the given guilds.
+
+        Args:
+            self (object): _description_
+            guilds (tuple): Guilds who's gonna receive the message
+            messages (tuple): Messages who's gonna be sent to the guilds
+        """
         for guild in guilds:
             event_channel_id = self.database.get_data(f'bot:guilds:{guild.name}:event_channel')
             channel = guild.get_channel(int(event_channel_id))
             for message in messages:
                 await channel.send(embed=message)
-        
-    def prepare_guilds_for_action(self: object, bot):
-        guilds = self.get_all_guilds()
-        return tuple(bot.get_guild(int(guild_id)) for guild_id in guilds.values())
-        
-    def get_all_guilds(self: object) -> tuple:
-        return self.database.get_data_list('bot:guilds:*')
+                
     
 if __name__ == '__main__':
     FateEventsController.get_events()
+    
